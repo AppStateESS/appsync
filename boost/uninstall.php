6998176a895528fa2c5fd99d7c2dad2f5ff8f658
Matthew McNaney <mcnaney@gmail.com>
2016-05-31 10:43:44 -0400

    Added uninstall information.

--- boost/uninstall.php
+++ boost/uninstall.php
@@ -1,2 +1,12 @@
 <?php
 
+$db = \phpws2\Database::getDB();
+$db->exec('drop table if exists appsync_log_entry');
+$db->exec('drop sequence if exists appsync_log_entry_seq');
+$db->exec('drop table if exists appsync_portal');
+$db->exec('drop table if exists appsync_settings');
+$db->exec('drop sequence if exists appsync_settings_seq');
+$db->exec('drop table if exists appsync_umbrella_admin');
+$db->exec('drop sequence if exists appsync_umbrella_admin_seq');
+$db->exec('drop table if exists appsync_umbrella');
+$db->exec('drop sequence if exists appsync_umbrella_seq');
